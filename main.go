fa11b89e58e92610c4ffe3945e50f239d1c6ce7b
Thet Naing Tun <thetnaingtun@bindez.com>
2021-07-14 14:43:56 +0630

    set up route for thread

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"diocforum/auth"
 	"diocforum/post"
+	"diocforum/thread"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,10 @@ import (
 var apiBase = "/api"
 
 func main() {
+	thread.SetupRoute(apiBase)
 	post.SetupRoute(apiBase)
 	auth.SetUpRoute(apiBase)
 
+	log.Println("Server listening at :5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }

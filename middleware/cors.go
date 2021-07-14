267c6fdb521b4340a87eeee247311e130cae2971
Thet Naing Tun <thetnaingtun@bindez.com>
2021-07-14 22:05:38 +0630

    add some cors header

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,7 @@ func CORS(handler http.Handler) http.Handler {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers, X-Requested-With")
 		handler.ServeHTTP(w, r)
 	})
 }
